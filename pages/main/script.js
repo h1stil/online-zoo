a715800d433f432f197e20f15bf8723298ba86e3
h1stil <jkaem111@gmail.com>
2022-10-10 23:39:28 +0300

    feat: add pet carousel

--- pages/main/script.js
+++ pages/main/script.js
@@ -34,14 +34,6 @@ recallBtn.forEach((item, i) => {
   });
 });
 
-// const popup = document.querySelector(".container__item");
-
-// document.onclick = function (e) {
-//   if (e.target.className != "active") {
-//     popup.classList.toggle("active");
-//   }
-// };
-
 // TESTIMOTIONALS SWIPER
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -80,4 +72,116 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 
   testContainer.addEventListener("mousedown", mouseDownHandler);
+});
+
+// Pets Carousel
+
+const prevBtn = document.querySelector(".arrow_left");
+const nextBtn = document.querySelector(".arrow_right");
+const swiper = document.querySelector(".animals__swiper");
+const containerLeft = document.querySelector(".animals__container_left");
+const containerMain = document.querySelector(".animals__container");
+const containerRight = document.querySelector(".animals__container_right");
+const card1 = document.querySelector(".card1");
+const card2 = document.querySelector(".card2");
+const card3 = document.querySelector(".card3");
+const card4 = document.querySelector(".card4");
+const card5 = document.querySelector(".card5");
+const card6 = document.querySelector(".card6");
+const card7 = document.querySelector(".card7");
+const cards = [card1, card2, card3, card4, card5, card6, card7];
+
+const moveLeft = () => {
+  if (window.innerWidth > 1550) swiper.classList.add("transition-left_desctop");
+  if (window.innerWidth > 1200 && window.innerWidth <= 1550)
+    swiper.classList.add("transition-left_normal");
+  if (window.innerWidth > 980 && window.innerWidth <= 1200)
+    swiper.classList.add("transition-left_small");
+  if (window.innerWidth <= 980) swiper.classList.add("transition-left_mobile");
+  nextBtn.setAttribute("disabled", "true");
+  prevBtn.setAttribute("disabled", "true");
+};
+
+const moveRight = () => {
+  if (window.innerWidth > 1550)
+    swiper.classList.add("transition-right_desctop");
+  if (window.innerWidth > 1200 && window.innerWidth <= 1550)
+    swiper.classList.add("transition-right_normal");
+  if (window.innerWidth > 980 && window.innerWidth <= 1200)
+    swiper.classList.add("transition-right_small");
+  if (window.innerWidth <= 980) swiper.classList.add("transition-right_mobile");
+  nextBtn.setAttribute("disabled", "true");
+  prevBtn.setAttribute("disabled", "true");
+};
+
+const generateLeft = () => {
+  let randomCards = [];
+  while (randomCards.length < 6) {
+    let num = Math.floor(Math.random() * 7);
+    randomCards.push(cards[num]);
+    randomCards = [...new Set(randomCards)];
+  }
+  containerLeft.innerHTML = "";
+  containerLeft.appendChild(randomCards[0]);
+  containerLeft.appendChild(randomCards[1]);
+  containerLeft.appendChild(randomCards[2]);
+  containerLeft.appendChild(randomCards[3]);
+  containerLeft.appendChild(randomCards[4]);
+  containerLeft.appendChild(randomCards[5]);
+};
+
+const generateRight = () => {
+  let randomCards = [];
+  while (randomCards.length < 6) {
+    let num = Math.floor(Math.random() * 7);
+    randomCards.push(cards[num]);
+    randomCards = [...new Set(randomCards)];
+  }
+  containerRight.innerHTML = "";
+  containerRight.appendChild(randomCards[0]);
+  containerRight.appendChild(randomCards[1]);
+  containerRight.appendChild(randomCards[2]);
+  containerRight.appendChild(randomCards[3]);
+  containerRight.appendChild(randomCards[4]);
+  containerRight.appendChild(randomCards[5]);
+};
+
+prevBtn.addEventListener("click", () => {
+  generateLeft();
+  moveLeft();
+});
+
+nextBtn.addEventListener("click", () => {
+  generateRight();
+  moveRight();
+});
+
+swiper.addEventListener("animationend", (event) => {
+  if (
+    event.animationName === "move-left_desctop" ||
+    event.animationName === "move-left_normal" ||
+    event.animationName === "move-left_small" ||
+    event.animationName === "move-left_mobile"
+  ) {
+    swiper.classList.remove("transition-left_desctop");
+    swiper.classList.remove("transition-left_normal");
+    swiper.classList.remove("transition-left_small");
+    swiper.classList.remove("transition-left_mobile");
+    containerRight.innerHTML = containerLeft.innerHTML;
+    containerMain.innerHTML = containerLeft.innerHTML;
+  } else if (
+    event.animationName === "move-right_desctop" ||
+    event.animationName === "move-right_normal" ||
+    event.animationName === "move-right_small" ||
+    event.animationName === "move-right_mobile"
+  ) {
+    swiper.classList.remove("transition-right_desctop");
+    swiper.classList.remove("transition-right_normal");
+    swiper.classList.remove("transition-right_small");
+    swiper.classList.remove("transition-right_mobile");
+    containerLeft.innerHTML = containerRight.innerHTML;
+    containerMain.innerHTML = containerRight.innerHTML;
+  }
+  prevBtn.removeAttribute("disabled");
+  nextBtn.removeAttribute("disabled");
 });
